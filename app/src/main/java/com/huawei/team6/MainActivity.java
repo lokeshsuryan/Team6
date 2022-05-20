6065bb7e9c0105f7e721e830c74e458b62c97cb3
Lokesh Kumar <lokesh.kumar4@huawei.com>
2022-05-20 10:46:40 +0530

    Done some changes into mainActivity

--- app/src/main/java/com/huawei/team6/MainActivity.java
+++ app/src/main/java/com/huawei/team6/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+// This my commentebnxnns
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
